ea7b58740c7abb634f4380e09f5cf98b71f886dc
Andres <ricardo.gomez.restrepo@gmail.com>
2024-02-27 18:52:49 -0500

    Added a new property to connection object

--- models/mysql/db/config.js
+++ models/mysql/db/config.js
@@ -3,3 +3,4 @@ export const DB_USER = process.env.DB_USER ?? "root";
 export const DB_PORT = process.env.DB_PORT ?? 3306;
 export const DB_PASSWORD = process.env.DB_PASSWORD ?? "";
 export const DB_NAME = process.env.DB_NAME ?? "moviesdb";
+export const DB_URI = process.env.DB_NAME ?? "";
