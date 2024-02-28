c80f55e07881f1a4277977714e6073358bb7684a
Andres <ricardo.gomez.restrepo@gmail.com>
2024-02-27 19:03:44 -0500

    minor fixes

--- models/mysql/db/connection.js
+++ models/mysql/db/connection.js
@@ -1,5 +1,12 @@
 import { createPool } from "mysql2/promise";
-import { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER } from "./config.js";
+import {
+  DB_HOST,
+  DB_NAME,
+  DB_PASSWORD,
+  DB_PORT,
+  DB_USER,
+  DB_URI,
+} from "./config.js";
 
 export const pool = createPool({
   host: DB_HOST,
